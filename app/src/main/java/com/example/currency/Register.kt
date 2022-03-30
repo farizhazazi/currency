d94d79bb90bf9798ad256f310915076af8f1eee3
raveenaaaaaa <raveena25301@gmail.com>
2022-03-31 02:56:54 +0700

    notif

--- app/src/main/java/com/example/currency/Register.kt
+++ app/src/main/java/com/example/currency/Register.kt
@@ -55,29 +55,29 @@ class Register : AppCompatActivity() {
             //openMyActivity3()
 
         }
-        fun createNotificationChannel() {
-            // Create the NotificationChannel, but only on API 26+ because
-            // the NotificationChannel class is new and not in the support library
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                val name = getString(R.string.channel_name)
-                val descriptionText = getString(R.string.channel_description)
-                val importance = NotificationManager.IMPORTANCE_DEFAULT
-                val channel = NotificationChannel("channel 1", name, importance).apply {
-                    description = descriptionText
-                }
-                // Register the channel with the system
-                val notificationManager: NotificationManager =
-                    getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-                notificationManager.createNotificationChannel(channel)
-            }
-        }
 
     }
-
 
     private fun createNotificationChannel() {
-        TODO("Not yet implemented")
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("channel 1", name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
+
+    //private fun createNotificationChannel() {
+      //  TODO("Not yet implemented")
+    //}
 
     private fun openMyActivity2() {
         val intentmasuk2 = Intent(this@Register, Login::class.java)
